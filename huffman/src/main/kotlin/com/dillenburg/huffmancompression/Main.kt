955a0c163703928ddaff6babf90138c0561cafc4
Luca Dillenburg <luca.assumpcao.dillenburg@gmail.com>
2020-12-01 09:16:16 -0300

    :sparkles: FEATURE: Create CTL for Huffman compression
    Signed-off-by: Luca Dillenburg <luca.assumpcao.dillenburg@gmail.com>

--- huffman/src/main/kotlin/com/dillenburg/huffmancompression/Main.kt
+++ huffman/src/main/kotlin/com/dillenburg/huffmancompression/Main.kt
@@ -3,36 +3,61 @@ package com.dillenburg.huffmancompression
 import com.dillenburg.huffmancompression.compressor.FileCompressor
 import com.dillenburg.huffmancompression.compressor.FileDecompressor
 import java.io.File
+import java.nio.file.Files
+import java.nio.file.Paths
+import kotlin.system.exitProcess
+
+fun getFileSize(fileName: String): Long {
+    val path = Paths.get(fileName)
+    return Files.size(path)
+}
+
+fun printSizeChange(inputSize: Long, outputSize: Long): Boolean {
+    val rate = outputSize.toDouble() / inputSize.toDouble()
+    if (rate < 1) {
+        val decreased_perc = 1 - rate
+        print("${"%.2f".format(decreased_perc * 100f)} decrease in size")
+        return false
+    } else {
+        val increased_perc = rate - 1
+        print("${"%.2f".format(increased_perc * 100f)} increase in size")
+        return true
+    }
+}
 
 fun main(args: Array<String>) {
     try {
-        println(" ## MENU ## ")
-        println("1. Compress file")
-        println("2. Decompress file")
+        if (args.size < 2) {
+            println("Missing arguments! The program expects two arguments in this order: the mode (compress or decompress) and the file name. \n")
+            exitProcess(1)
+        }
 
-        print("Which option do you choose? ")
-        val option = readLine()
+        val mode = args.get(0)
+        if (mode != "compress" && mode != "decompress") {
+            println("Unknown mode '$mode'. Available modes: 'compress' and 'decompress'.")
+            exitProcess(2)
+        }
 
-        if (option != "1" && option != "2")
-            return
+        val inputPath = args.get(1)
 
-        print("Write the path to the file you want to compress: ")
-        val path = readLine()
-
-        var newPath: String
-        if (option == "1") {
-            val fileCompressor = FileCompressor(File(path))
+        var outputPath: String
+        if (mode == "compress") {
+            val fileCompressor = FileCompressor(File(inputPath))
             fileCompressor.compress()
-            newPath = fileCompressor.writeToFile()
-        }
-        else {
-            val fileDecompressor = FileDecompressor(File(path))
+            outputPath = fileCompressor.writeToFile()
+        } else {
+            val fileDecompressor = FileDecompressor(File(inputPath))
             fileDecompressor.decompress()
-            newPath = fileDecompressor.writeToFile()
+            outputPath = fileDecompressor.writeToFile()
         }
 
-        print("The file was successfully ${if (option=="1") "" else "de"}compressed. The name of the new file is $newPath")
+        print("The file $inputPath was successfully ${mode}ed to $outputPath with a ")
+        val increased = printSizeChange(getFileSize(inputPath), getFileSize(outputPath))
+        print(".")
+        if (increased && mode == "compress")
+            print(" The compression resulted in a larger file! You should use the original file!")
     } catch (e: Exception) {
         print("Error: $e")
+        exitProcess(3)
     }
 }
